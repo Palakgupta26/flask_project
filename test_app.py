7d5708e91243105891fd0a099b8e295de70d0eaf
Palakgupta26 <Palakg431@gmail.com>
2025-02-01 18:41:27 +0530

    changed

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import app
 
 def test_home():
-    reponse=app.test_client().get("/")
+    response=app.test_client().get("/")
 
     assert response.status_code==200
     assert response.data ==b"Hello jolly"
